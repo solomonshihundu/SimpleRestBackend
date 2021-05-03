c509264350693d6b651ab230b5ba04867f787ae0
Solo <shihundusolomon@gmail.com>
2021-05-03 18:14:31 +0300

    initial commit

---
+++ src/main/java/com/ss/rest/api/simplebackend/SimpleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ss.rest.api.simplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/rest/api/simplebackend/SimpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.rest.api.simplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
