4c0d22a2b3b74b6c1d095f9254d8938faa1cecf9
solomonshihundu <shihundusolomon@gmail.com>
2021-05-13 05:37:49 +0300

    responses

---
+++ src/main/java/com/ss/rest/api/simplebackend/payload/response/MessageResponse.java
@@ -0,0 +1,18 @@
+package com.ss.rest.api.simplebackend.payload.response;
+
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
